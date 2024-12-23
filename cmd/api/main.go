c651487e7956db8ca8303e5b0a9798d32340f601
NucleonGodX <racerpro41@gmail.com>
2024-12-23 13:49:00 +0530

    initial push

---
+++ cmd/api/main.go
@@ -0,0 +1,11 @@
+package main
+
+import(
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/NucleonGodX/Distributed-Task-Queue-System/internal/handlers"
+	log "github.com/sirupsen/logrus"
+
+)
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/NucleonGodX/Distributed-Task-Queue-System
+
+go 1.23.4
